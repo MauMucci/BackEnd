afd3eff4747b754590d82a4e2e427691f20de74d
Mauro Mucci <mauromucci@hotmail.com>
2023-04-02 19:26:34 -0300

    falta resolver que compare code

--- clase-2.js
+++ clase-2.js
@@ -25,9 +25,25 @@ class ProductManager {
             const lastProduct = this.products [this.products.length-1] 
             product.id = lastProduct.id + 1; 
         }
+        
 
-        this.products.push(product)
+        
+        //this.products.push(product)
+
+        this.products.forEach(p => {                
+            //console.log(p.code)  
+            if(p.code == product.code){
+            console.log("adentro " + p.code + " " + product.code)   
+            this.products.push(product) }
+            else{
+                console.log("no entre")
+            }                                  
+                           
+        })
+
+        
        
+        
     }
 
     getProducts = () => {
@@ -38,16 +54,16 @@ class ProductManager {
        return console.log(this.products);
     }
 
-    getProductById = (c) => {
-        let codeOK
+    getProductById = (id) => {
+        let idCheck
         let requestedProduct
         this.products.forEach(p =>{
-            if(p.code === c){
-                codeOK = c
+            if(p.id === id){
+                idCheck = id
                 requestedProduct = p
             }
         })
-        if(codeOK)
+        if(idCheck)
             {
                 console.log("Producto solicitado: ",requestedProduct)
             }    
@@ -58,28 +74,32 @@ class ProductManager {
     let productManager = new ProductManager()
 
     const prod1 = {
-        title:"producto prueba",
+        title:"producto 1",
         description: "este e sun producto prueba",
+        price: 200,
+        thumbnail: "Sin imagen",
+        code: "AAA",
+        stock: 25,        
+    }
+    const prod2 = {
+        title:"producto 2",
+        description: "este e sun producto pasdasdasdasdasdasdasdsadrueba",
         price: 200,
         thumbnail: "Sin imagen",
         code: "abc1232",
         stock: 25,        
     }
-   /*  const prod2 = {
-        title:"producto prueba",
-        description: "este e sun producto pasdasdasdasdasdasdasdsadrueba",
-        price: 200,
-        thumbnail: "Sin imagen",
-        code: "abc123",
-        stock: 25,        
-    } */
+   
 
 
     productManager.addProducts(prod1)
-    //productManager.addProducts(prod2) 
+    productManager.addProducts(prod2) 
+
+    //productManager.getProductById(1)
+    //productManager.addProducts(prod3) 
 
     //console.log(prod1)
     //console.log(productManager.getProducts())
   /*   productManager.getProductById(2) */
 
-    productManager.getProducts()
+    //productManager.getProducts()
