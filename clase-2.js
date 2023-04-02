641669cb86d5baab337674bf7f5269d307e0b15c
Mauro Mucci <mauromucci@hotmail.com>
2023-04-02 17:30:00 -0300

    primer desafio

---
+++ clase-2.js
@@ -0,0 +1,85 @@
+class ProductManager {      
+      
+    constructor(){                   
+        this.products = []       
+    }
+    
+    addProducts = ({title,description,price,thumbnail,code,stock}) =>{
+        if(!title||!description || !price || !thumbnail || !code || !stock){
+            console.log("datos incompletos")
+            return null
+        }
+
+        const product = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        }
+
+        if(this.products.length === 0){//No hay productos en el arreglo => le asignamos el id 1
+            product.id = 1;
+        }else{ 
+            const lastProduct = this.products [this.products.length-1] 
+            product.id = lastProduct.id + 1; 
+        }
+
+        this.products.push(product)
+       
+    }
+
+    getProducts = () => {
+
+        if(this.products.length == 0){
+            console.log("No hay productos")
+        }
+       return console.log(this.products);
+    }
+
+    getProductById = (c) => {
+        let codeOK
+        let requestedProduct
+        this.products.forEach(p =>{
+            if(p.code === c){
+                codeOK = c
+                requestedProduct = p
+            }
+        })
+        if(codeOK)
+            {
+                console.log("Producto solicitado: ",requestedProduct)
+            }    
+    }
+    
+}
+
+    let productManager = new ProductManager()
+
+    const prod1 = {
+        title:"producto prueba",
+        description: "este e sun producto prueba",
+        price: 200,
+        thumbnail: "Sin imagen",
+        code: "abc1232",
+        stock: 25,        
+    }
+   /*  const prod2 = {
+        title:"producto prueba",
+        description: "este e sun producto pasdasdasdasdasdasdasdsadrueba",
+        price: 200,
+        thumbnail: "Sin imagen",
+        code: "abc123",
+        stock: 25,        
+    } */
+
+
+    productManager.addProducts(prod1)
+    //productManager.addProducts(prod2) 
+
+    //console.log(prod1)
+    //console.log(productManager.getProducts())
+  /*   productManager.getProductById(2) */
+
+    productManager.getProducts()
